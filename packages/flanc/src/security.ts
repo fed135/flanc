3c12c8378524c11dd02351dbf3903bb7623700cd
Frederic Charette <fred@Frederics-MacBook-Pro.local>
2021-07-14 18:04:52 -0400

    bumped versions

--- packages/flanc/src/security.ts
+++ packages/flanc/src/security.ts
@@ -2,6 +2,8 @@
 import { ExpressNext, ExpressRequest, ExpressResponse } from 'flanc/express-types';
 
 export default function security(req: ExpressRequest, res: ExpressResponse, next: ExpressNext) {
+  res.removeHeader('X-Powered-By');
+
   if (decodeURIComponent(req.url).includes('<script>')) {
     return res.status(406).end('Illegal component in URI');
   }
